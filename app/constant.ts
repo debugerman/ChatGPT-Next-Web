d1ec7f3cfae7893d6abb58273fd58d922a492925
debugerman <debugerman@users.noreply.github.com>
2025-04-13 18:47:08 +0800

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -611,6 +611,10 @@ const xAIModes = [
   "grok-2-vision-1212",
   "grok-2-vision",
   "grok-2-vision-latest",
+  "grok-3-beta",
+  "grok-3-fast-beta",
+  "grok-3-mini-beta",
+  "grok-3-mini-fast-beta",
 ];
 
 const chatglmModels = [
